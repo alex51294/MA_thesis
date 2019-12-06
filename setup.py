672d2146ee59a8bca3908580cce9fe9a6f002cbb
alex51294 <58595720+alex51294@users.noreply.github.com>
2019-12-06 14:07:48 +0100

    Add files via upload
    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MA_thesis',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
